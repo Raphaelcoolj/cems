0ae6232b43aade4ef3da799a71be21aa53e26935
Emeka <ekehchukwuemeka375@gmail.com>
2026-03-05 12:18:49 +0100

    third commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token,
+  },
   pages: {
     signIn: "/admin/login",
   },
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export const config = {
